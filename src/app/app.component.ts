4ccba4e454f9e6dd17d547f979eded0584916535
rOBI <robertaliaj97@gmail.com>
2023-03-16 11:13:19 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'to-do-list';
+
+  todos$: Observable<any>; // ein Observable ist eine Variable die sich updatet //der DatenTyp "any" ist weil die Variable ein Json ist (json von der Datenbank)
+  // das "$"-Zeichen dazu wird verwendet um zu wissen das es sich um eine variable handelt die sich regelmäßig gupdatet
+
+  constructor(firestore: Firestore) {             // mit dem Befehlt "firestore: Firestore" wird Firestore importiert
+    const coll = collection(firestore, 'todos');  // greift auf die collection in Firestore an der Stelle "todos"
+    this.todos$ = collectionData(coll);           // mit "collectionData(coll);" werden die Daten aus der Datenbank "todos" geholt und der Variable "todos$" zugewiesen
+    
+    this.todos$.subscribe( (newTodos$)=> {                 // subscribe (auf deutsch abonnieren) wird verwendet wenn man aus der Datenbank Daten holen möchte die sich regelmäßig ändern
+      console.log('neue Todos sind:',newTodos$);
+    });                                           // die "()=>{}" Funktion wird jedes mal aufgerufen wenn die Daten auf der Datenbank sich ändern
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({})),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent]
